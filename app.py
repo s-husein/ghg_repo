b4c171e34dace3c77f805667c90cd7d9a42a2cf5
s-husein <shahrukhh442@gmail.com>
2024-10-05 11:03:23 +0500

    -

---
+++ app.py
@@ -0,0 +1,10 @@
+import requests
+import folium
+import folium.plugins
+from folium import Map, TileLayer
+from pystac_client import Client
+import branca
+import pandas as pd
+import matplotlib.pyplot as plt
+from flask import Flask, send_from_directory
+
